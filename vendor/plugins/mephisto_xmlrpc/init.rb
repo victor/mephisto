ea152360b4c5269020fda174ca47b58bf201fe26
Victor Jalencas <victor+github@carotena.net>
2010-02-24 09:03:13 +0100

    Add mephisto_xmlrpc plugin

---
+++ vendor/plugins/mephisto_xmlrpc/init.rb
@@ -0,0 +1,11 @@
+
+# require 'action_web_service'
+
+lib_path = File.join(directory, 'lib', 'apis')
+ ActiveSupport::Dependencies.load_paths << lib_path
+
+ ActiveSupport::Dependencies.load_once_paths.delete File.dirname(__FILE__) + "/app/controllers"
+
+
+
+ 
